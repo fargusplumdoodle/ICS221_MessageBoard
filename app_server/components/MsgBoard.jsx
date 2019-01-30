451cb2f1e4b0f8c4d21b44e2cf681664eb9fbb8d
Isaac Thiessen <isaac.thiessen31@online.camosun.ca>
2019-01-30 10:08:18 -0800

    finished message board component with addMessage method

--- app_server/components/MsgBoard.jsx
+++ app_server/components/MsgBoard.jsx
@@ -1,17 +1,72 @@
 const React = require('react');
 const MsgList = require('./MsgList.jsx');
+const NewMsg = require('./NewMsg.jsx');
 
 class MsgBoard extends React.Component {
 	constructor(props){
 		super(props);
+		this.addMessage = this.addMessage.bind(this);
 		this.state = {
 			messages: this.props.messages,
 		}
 	}
 
+    handleHTTPErrors(response) {
+        if (!response.ok) throw Error(response.status + ': ' + response.statusText);
+        return response;
+    }
+
+    componentDidMount() {
+	    console.log('ComponentDidMount is Executed');
+        fetch('http://localhost:3003/msgs')
+            .then(response=> this.handleHTTPErrors(response))
+            .then(response=> response.json())
+            .then(result=> {
+                console.log(result);
+                this.setState({
+                    msg: result.msg,
+                    name: result.name,
+                });
+            })
+            .catch(error=> {
+                console.log('Fetch API Error: ' + error);
+            });
+    }
+
+    addMessage(message){
+        let msgs = this.state.messages;
+
+        // add id attribute
+        message.id = msgs.length;
+
+        // append to array
+        msgs.push(message);
+
+        // update state var
+        this.setState({
+            messages: msgs
+        });
+
+        // update back-end data
+        fetch('http://localhost:3003/msgs', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(message)
+        })
+            .then(response => this.handleHTTPErrors(response))
+            .catch(error => {
+                console.log(error);
+            });
+	}
+
 	render(){
 	    return (
-	    	<MsgList messages={this.state.messages}/>
+	    	<div>
+                <NewMsg addMsgCallback={this.addMessage}/>
+                <MsgList messages={this.state.messages}/>
+			</div>
 		)
 	}
 }
