6ee4402d9a7a839332951b84139f044ee48214a8
Isaac Thiessen <isaac.thiessen31@online.camosun.ca>
2019-01-30 10:21:05 -0800

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, 'client_side', 'app.jsx'),
+    output: {
+        path: path.join(__dirname, '/public/javascripts/'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-react']
+                }
+            }
+        ]
+    },
+    mode: process.env.NODE_ENV || 'development'
+};
