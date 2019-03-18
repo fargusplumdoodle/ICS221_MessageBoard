3f4e11fed13abfe6aa4545062a39b248205ecb63
fargus plumdoodle <dont@worry.about.it>
2019-03-18 13:09:10 -0700

    task 4 complete, users can be created

--- api_server/controllers/user-api.js
+++ api_server/controllers/user-api.js
@@ -2,8 +2,32 @@ const mongoose = require('mongoose');
 const userModel = mongoose.model('user');
 
 const registerNewUser = (req, res) => {
-    res.status(200).send('Successful API New User POST Request');
+//     res.status(200).send('Successful API New User POST Request');
+    userModel
+        .findOne({
+            email: req.body.email
+        }, (err, user) => {
+            if (err) {
+                res.status(404).json(err);
+                return;
+            }
+            if (user) {
+                res
+                    .status(403)
+                    .json({"api-msg": "email already exists"});
+                return;
+            }
+            userModel
+                .create(req.body, (err, user) => {
+                    if (err) {
+                        res.status(400).json(err);
+                    } else {
+                        res.status(201).json(user);
+                    }
+                });
+        });
 };
+
 const loginUser = (req, res) => {
     res.status(200).send('Successful API Login User GET Request');
 };
