23ed72b2d18652b5e161aa5f246db294e881f5ab
fargus plumdoodle <dont@worry.about.it>
2019-03-18 10:52:24 -0700

    added routing for users, task 3 complete

--- api_server/routes/api_router.js
+++ api_server/routes/api_router.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const msgAPIController = require('../controllers/msg-api');
+const userAPIController = require('../controllers/user-api');
+
+router.post('/users', userAPIController.registerNewUser);
+router.get('/users/login', userAPIController.loginUser);
 
 router.route('/msgs')
     .get(msgAPIController.getAllMessagesOrderedByLastPosted)
