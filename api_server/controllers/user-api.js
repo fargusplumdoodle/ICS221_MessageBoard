225e40b48ce8ef72e7c23fc613622705ee811b2d
fargus plumdoodle <dont@worry.about.it>
2019-03-18 12:57:03 -0700

    finished task 3

---
+++ api_server/controllers/user-api.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const userModel = mongoose.model('user');
+
+const registerNewUser = (req, res) => {
+    res.status(200).send('Successful API New User POST Request');
+};
+const loginUser = (req, res) => {
+    res.status(200).send('Successful API Login User GET Request');
+};
+
+module.exports = {
+    loginUser,
+    registerNewUser
+};
--- api_server/db.js
+++ api_server/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 let dbURI = 'mongodb://localhost:27017/msgsdb';
 //mongoose.connect(dbURI, { useNewUrlParser: true });
-mongoose.connect(dbURI, { useNewUrlParser: true , useCreateIndex: true});
+mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true });
 
 mongoose.connection.on('connected', () => {
 	console.log('Mongoose connected to ' + dbURI);
